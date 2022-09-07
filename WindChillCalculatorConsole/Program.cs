d0fb40c294f12bd8156bb33f5bd732a345daed2a
Lars Nysom <LANY@ucn.dk>
2022-09-07 12:20:23 +0200

    Added base functionality, so the solution can build

--- WindChillCalculatorConsole/Program.cs
+++ WindChillCalculatorConsole/Program.cs
@@ -1,8 +1,15 @@
-﻿
-// TODO: Utilize the MeteorologyCalculator class library to calculate the wind chill factor of a given temperature and wind speed.
+﻿using System;
 
-// HINT: The code to call the wind chill calculator could look something like this 
+Console.WriteLine("Windchill Calculator v1.0");
+Console.WriteLine();
 
+
+// TODO: Utilize the MeteorologyCalculator class library to
+//       calculate the wind chill factor of a given temperature
+//       and wind speed.
+
+// HINT: The code to call the wind chill calculator could look
+//       something like this:
 // Temperature temperature = new Temperature(5, Temperature.Scale.Celsius);
 // WindSpeed speed = new WindSpeed(9, WindSpeed.Unit.MilesPerHour);
 // WindChillFactor wcf = new WindChillFactor(temperature, speed);
